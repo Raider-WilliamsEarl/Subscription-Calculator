a63c56fc295a6db8286e4ee5ea8cc07d0f19e0c4
Raider Williams-Earl <samwilliamsearl@gmail.com>
2025-01-27 11:23:17 -0700

    programmed subscription calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-// Your JS code here
+//**Select Elements**//
+//Global Variables//
+const subTypeElement = document.querySelector("#subscription");
+const subDurationElement = document.querySelector("#months");
+const result = document.querySelector(".result");
+let subType = "basic";
+let subDuration = 1;
+
+//**Event Listeners**//
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  updateSubscriptionDiv()
+});
+subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value);
+    updateSubscriptionDiv()
+});
+
+//**Calculation Function**//
+const updateSubscriptionDiv = function() {
+    let monthlyCost = 5;
+    if (subType === "standard") {
+        monthlyCost = 7;
+    } else if (subDurationElement === "premium") {
+        monthlyCost = 10;
+    }
+    const total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}`;
+};
